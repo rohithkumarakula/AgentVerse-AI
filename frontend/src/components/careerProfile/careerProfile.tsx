e8fbcbb8733d0f2002211d14b76cc14ebb807184
rohith <AKULAROHITHKL199@GMAIL.COM>
2026-09-01 16:35:01 +0530

    Fix CareerProfile CSS import

--- frontend/src/components/careerProfile/careerProfile.tsx
+++ frontend/src/components/careerProfile/careerProfile.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import "./CareerProfile.css";
+import "./careerProfile.css";
 
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
